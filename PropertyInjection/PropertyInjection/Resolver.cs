ca443e0d34fecfd932084c8b8c174676c55b8cbf
Cory Cook <corycook@ieee.org>
2015-03-23 04:03:01 +0000

    Initial Commit

---
+++ PropertyInjection/PropertyInjection/Resolver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace PropertyInjection
+{
+	public static class Resolver<T>
+	{
+		class PKey
+		{
+			Type Type { get; set; }
+			string PropertyName {get;set;}
+
+			public PKey(Type type, string propertyName)
+			{
+				Type = type;
+				PropertyName = propertyName;
+			}
+
+			public class PKeyComparer : IEqualityComparer<PKey>
+			{
+				#region IEqualityComparer implementation
+				public bool Equals (PKey x, PKey y)
+				{
+					return x.Type == y.Type && x.PropertyName == y.PropertyName;
+				}
+				public int GetHashCode (PKey obj)
+				{
+					return obj.PropertyName.GetHashCode () + obj.Type.GetHashCode ();
+				}
+				#endregion
+			}
+		}
+		static readonly Dictionary<PKey, Expression> _getters;
+
+		static Resolver()
+		{
+			_getters = new Dictionary<PKey, Expression> (new PKey.PKeyComparer ());
+		}
+
+		public static void Register<TIn, TOut>(Expression<Func<T, TOut>> property, Expression<Func<TIn, TOut>> getter)
+		{
+			var name = property.Body as MemberExpression;
+			_getters.Add (new PKey (typeof(TIn), name.Member.Name), getter);
+		}
+
+		public static TOut Resolve<TIn, TOut>(TIn target, Expression<Func<T, TOut>> property)
+		{
+			var name = property.Body as MemberExpression;
+			var key = new PKey (target.GetType(), name.Member.Name);
+			Expression e;
+			if (_getters.TryGetValue(key, out e)) {
+				var param = Expression.Parameter (typeof(TIn), "x");
+				var expression = Expression.Invoke (e, Expression.Convert (param, target.GetType ()));
+				var lambda = Expression.Lambda<Func<TIn, TOut>> (expression, param);
+				var compiled = lambda.Compile ();
+				return compiled.Invoke (target);
+			}
+			return default(TOut);
+		}
+	}
+}
+
---
+++ TestProperyInjection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PropertyInjection;
+using System.Linq.Expressions;
+using System.Dynamic;
+
+namespace TestProperyInjection
+{
+	interface RC 
+	{
+		int Double { get; set; }
+		string String { get; set; }
+	}
+
+	interface RD
+	{
+		int Double { get; }
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Resolver<RC>.Register<string, int> (n => n.Double, n => n.Length);
+			Resolver<RC>.Register<int, int> (n => n.Double, n => n * 2);
+			Resolver<RC>.Register<double, int> (n => n.Double, n => (int)(n * n));
+			Resolver<RC>.Register<string, string> (n => n.String, n => string.Format("string({0})", n));
+			Resolver<RC>.Register<int, string> (n => n.String, n => string.Format ("int({0})", n));
+			Resolver<RC>.Register<double, string> (n => n.String, n => string.Format ("double({0})", n));
+
+			Resolver<RD>.Register<string, int> (n => n.Double, n => 0);
+			Resolver<RD>.Register<int, int> (n => n.Double, n => 1);
+			Resolver<RD>.Register<double, int> (n => n.Double, n => 2);
+
+			var results = new object[] { 3, 1.8, "Test", 6, "Hello World!", 15, 6.5, 4.0 };
+			foreach (var i in results) {
+				var value = Resolver<RC>.Resolve (i, n => n.String);
+				Console.WriteLine("{0}: {1}", i.GetType(), value);
+			}
+		}
+	}
+}
